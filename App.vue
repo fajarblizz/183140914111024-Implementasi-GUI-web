3d2fd36168ee7a6514525ed084fe43b06af2d12e
fajarblizz <57934608+fajarblizz@users.noreply.github.com>
2020-02-03 22:13:56 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/kalku.png">
+    <HelloWorld :pesan="message"/>
+    <!-- <input v-model="message" placeholder="edit me"> -->
+    <!-- /*v-bind:id="message"*/ -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/Kalkulator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return ({
+      message: 'Kalkulator Sederhana'
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+  background-color: rgb(0, 0, 0);
+}
+</style>
